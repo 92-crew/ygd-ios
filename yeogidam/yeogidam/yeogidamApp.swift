54e05ecde816d131a0f76dd1e527f592e803c9d8
kangwooklee <kngwk.bsns@etoos.com>
2022-10-05 09:33:11 +0900

    Initialize Project

---
+++ yeogidam/yeogidam/yeogidamApp.swift
@@ -0,0 +1,17 @@
+//
+//  yeogidamApp.swift
+//  yeogidam
+//
+//  Created by Kangwook Lee on 2022/10/05.
+//
+
+import SwiftUI
+
+@main
+struct yeogidamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
